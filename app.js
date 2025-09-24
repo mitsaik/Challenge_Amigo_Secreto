664b0dd5b865e0773cfc10eabd25c34ea7634ac8
Isaac de Araújo <mitsaik.contas@gmail.com>
2025-09-23 22:51:18 -0300

    criando um alerta

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 //array para guardar os nomes
-let amigo = [];
+let amigos = [];
 
 //função para add os nomes
-function addAmigos() {
+function adicionarAmigo() {
     const acessoInput = document.getElementById('amigo'); // acesso ao <input>
     const nomes = acessoInput.value.trim(); // guarda o nome digitado e limpa o <input>
+
+    //alerta
+    if (nomes === '') {
+        alert("Por favor, insira um nome válido.");
+        return; 
+    }
+    
+    amigos.push(nomes);
+    acessoInput.value = '';
 }
 
 
